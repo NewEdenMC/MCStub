7635e0b6cadb7ebd9d1314d1e4bdb33fdc26904f
aaron <AaronMcHale>
2017-07-18 19:51:09 +0100

    Minor changes to match GameProfile class better

--- src/main/java/com/mojang/authlib/GameProfile.java
+++ src/main/java/com/mojang/authlib/GameProfile.java
@@ -4,14 +4,14 @@ import java.util.UUID;
 
 public class GameProfile {
 
-    private PropertyMap properties;
+    private final PropertyMap properties = new PropertyMap();
 
     public GameProfile(UUID uuid, String s) {
         // stub method
     }
 
     public PropertyMap getProperties() {
-        return properties;
+        return this.properties;
     }
 
 }
